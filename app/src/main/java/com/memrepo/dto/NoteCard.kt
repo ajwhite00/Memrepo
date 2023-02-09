6a27622c7223e13eea71a65491d95f97e68c7e2d
patel6rv <patel6rv@mail.uc.edu>
2023-02-09 13:25:17 -0500

    Simplified variables and comments

--- app/src/main/java/com/memrepo/dto/NoteCard.kt
+++ app/src/main/java/com/memrepo/dto/NoteCard.kt
@@ -2,12 +2,10 @@ package com.memrepo.dto
 
 data class NoteCard(var cardID : Int, var title : String, var snippet : String){
 
-        /*hyphens can be used in words like self-evident or have a space after then hyphen in a sentence
-        As workaround to this, hyphenAdjustedSnippet will replace hyphens with a space character
-        Words with no space between hyphens will be properly separated and any instance with a double space will be removed by spacingAdjustedSnippet
-         */
-        var hyphenAdjustedSnippet = this.snippet.replace("-".toRegex(), " ")
-        var spacingAdjustedSnippet = hyphenAdjustedSnippet.replace("  ".toRegex(), " ")
-        var snippetDisplay = spacingAdjustedSnippet.replace("[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]".toRegex(), "")
-        var snippetDisplayList = snippetDisplay.split(" ").toList()
+        var snippetDisplay = this.snippet //has three rounds of replacement
+                .replace("-".toRegex(), " ") //replace hyphens with space because of two cases: split words with hyphen in between and remove hyphen in the middle of a sentence
+                .replace("  ".toRegex(), " ") //replace any double spaces caused by hyphen replacement with a single space
+                .replace("[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]".toRegex(), "") //remove punctuation and other non-letter chars
+
+        var snippetDisplayList = snippetDisplay.split(" ").toList() //create a list by splitting string on space
 }
