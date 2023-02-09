881f3d64381de28ce35c99614e34efe087da8965
patel6rv <patel6rv@mail.uc.edu>
2023-02-09 13:05:25 -0500

    Removed variable redundancy case in NoteCard DTO and added comments

--- app/src/main/java/com/memrepo/dto/NoteCard.kt
+++ app/src/main/java/com/memrepo/dto/NoteCard.kt
@@ -2,9 +2,12 @@ package com.memrepo.dto
 
 data class NoteCard(var cardID : Int, var title : String, var snippet : String){
 
+        /*hyphens can be used in words like self-evident or have a space after then hyphen in a sentence
+        As workaround to this, hyphenAdjustedSnippet will replace hyphens with a space character
+        Words with no space between hyphens will be properly separated and any instance with a double space will be removed by spacingAdjustedSnippet
+         */
         var hyphenAdjustedSnippet = this.snippet.replace("-".toRegex(), " ")
         var spacingAdjustedSnippet = hyphenAdjustedSnippet.replace("  ".toRegex(), " ")
-        var punctuationFreeSnippet = spacingAdjustedSnippet.replace("[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]".toRegex(), "")
-        var snippetDisplay = punctuationFreeSnippet
+        var snippetDisplay = spacingAdjustedSnippet.replace("[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]".toRegex(), "")
         var snippetDisplayList = snippetDisplay.split(" ").toList()
 }
