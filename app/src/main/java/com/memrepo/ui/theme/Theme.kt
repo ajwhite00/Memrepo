89f6e812aa7bfb141da518d886bc08388e589140
Pruthvi6797 <patel5pu@mail.uc.edu>
2023-02-21 22:48:08 -0500

    Color

--- app/src/main/java/com/memrepo/ui/theme/Theme.kt
+++ app/src/main/java/com/memrepo/ui/theme/Theme.kt
@@ -8,13 +8,13 @@ import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
-    primaryVariant = Purple700,
+    primaryVariant = Blue700,
     secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
+    primary = Blue500,
+    primaryVariant = Blue700,
     secondary = Teal200
 
     /* Other default colors to override
