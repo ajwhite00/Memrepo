1429fec99af17eeef87b43c78b7bd99247d36aef
Pruthvi6797 <patel5pu@mail.uc.edu>
2023-02-23 13:25:55 -0500

    AddFontError

--- app/src/main/java/com/memrepo/MainActivity.kt
+++ app/src/main/java/com/memrepo/MainActivity.kt
@@ -1,214 +1,56 @@
 package com.memrepo
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.TypedValue
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.memrepo.dto.NoteCard
 import com.memrepo.ui.theme.MemrepoTheme
-import kotlinx.coroutines.launch
-import java.util.*
 
 class MainActivity : ComponentActivity() {
-    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-            setContent {
-                MemrepoTheme {
-                    // A surface container using the 'background' color from the theme
-                    Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colors.background
-                    ) {
-                        //MainScreen()
-                        SpeechToText()
-                    }
-                }
-            }  
-    }
-    @ExperimentalMaterialApi
-    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-    @Composable
-    fun MainScreen() {
+        setContentView(R.layout.activity_main)
 
-        // Bottom Sheet is used to create a 'Modal' but for android apps
-        val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
-            bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed)
-        )
-        val coroutineScope = rememberCoroutineScope()
+        findViewById<TextView>(R.id.my_text_view).typeface = Res.Font(this, R.font.best)
+        findViewById<TextView>(R.id.my_text_view).setTextSize(TypedValue.COMPLEX_UNIT_SP, 50.toFloat())
 
-        BottomSheetScaffold(
-            scaffoldState = bottomSheetScaffoldState,
-            sheetContent = {
-                Box(
-                    Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight()
+        setContent {
+            MemrepoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
                 ) {
-                    Column {
-                        // Contents of the Bottom Sheet come from this component
-                        AddSnippet(bottomSheetScaffoldState)
-                    }
-                }
-            }, sheetPeekHeight = 0.dp
-        ) {
-            Scaffold(
-                // Bar across top of screen
-                topBar = {
-                    TopAppBar(
-                        title = {
-                            Text(
-                                "MemRepo"
-                            )
-                        },
-                    )
-                },
-                // Bar across bottom of screen
-                bottomBar = {
-                    BottomAppBar {
-                        Box(Modifier.fillMaxWidth()) {
-                            Button(
-                                onClick = {
-                                    // When the button is clicked the bottom sheet will expand or collapse
-                                    coroutineScope.launch {
-                                        if (bottomSheetScaffoldState.bottomSheetState.isCollapsed) {
-                                            bottomSheetScaffoldState.bottomSheetState.expand()
-                                        } else {
-                                            bottomSheetScaffoldState.bottomSheetState.collapse()
-                                        }
-                                    }
-                                },
-                                modifier = Modifier
-                                    .align(Alignment.Center)
-                                    .fillMaxWidth()
-                                    .fillMaxHeight()
-                            ) {
-                                (Text(text = "Add"))
-                            }
-                        }
-                    }
-                },
-                // The content in between the top bar and bottom bar
-                content = {
-                    MyContent()
-                }
-            )
-        }
-    }
-    @Composable
-    fun MyContent() {
-        val paddingModifier = Modifier.padding(10.dp)
-        Box(Modifier.fillMaxSize()) {
-            Card(
-                shape = RoundedCornerShape(20.dp),
-                elevation = 10.dp,
-                modifier = Modifier.padding(10.dp)
-            ) {
-                Column(paddingModifier) {
-                    // Title and Snippet are placeholders for now, eventually these will be injected values from the database
-                    Text(text = "Title", Modifier.fillMaxWidth())
-                    Text(text = "Snippet", Modifier.fillMaxWidth())
-                }
-                // Button will have the options to Edit or delete the note card
-                Button(modifier = Modifier.align(Alignment.TopEnd), onClick = {}){
-                    Text("...")
+                    Greeting("Android")
                 }
             }
         }
     }
+}
+/*val fontPath = "fonts/myfont.ttf"
+val typeface = Typeface.createFromAsset(context.assets, fontPath)
+textView.typeface = typeface*/
 
-    @OptIn(ExperimentalComposeUiApi::class)
-    @ExperimentalMaterialApi
-    @Composable
-    fun AddSnippet(bottomSheetScaffoldState: BottomSheetScaffoldState) {
-        var title by remember { mutableStateOf("") }
-        var text by remember { mutableStateOf("") }
-        val coroutineScope = rememberCoroutineScope()
-        val keyBoardController = LocalSoftwareKeyboardController.current
-        Box (modifier = Modifier.fillMaxWidth()) {
-            // When Exit button is clicked collapse the bottom sheet, hide the key board, and set the text fields to empty
-            Button(
-                onClick = {
-                    coroutineScope.launch { bottomSheetScaffoldState.bottomSheetState.collapse() }
-                    keyBoardController?.hide()
-                    title = ""
-                    text = ""
-                },
-                modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .padding(end = 10.dp)
-            ) {
-                Text("Exit")
-            }
-        }
-        Box (modifier = Modifier.fillMaxWidth()) {
-            Column (modifier = Modifier.align(Alignment.Center)){
-                OutlinedTextField(
-                    value = title,
-                    onValueChange = { title = it },
-                    label = { Text("Title") },
-                    modifier = Modifier
-                        .padding(bottom = 10.dp)
-                        .widthIn(max = 300.dp),
-                    singleLine = true
-                )
-                OutlinedTextField(
-                    value = text,
-                    onValueChange = { text = it },
-                    label = { Text("Snippet Text") },
-                    modifier = Modifier
-                        .height(300.dp)
-                        .widthIn(max = 300.dp)
-                )
-            }
-        }
-        Box ( modifier = Modifier.fillMaxWidth() ){
-            // When the Save button is clicked collapse the bottom sheet, hide the keyboard, save the fields to database
-            Button (
-                // Logic to save snippet
-                /* TODO */
-                onClick = {
-                    coroutineScope.launch { bottomSheetScaffoldState.bottomSheetState.collapse() }
-                    keyBoardController?.hide()
-                },
-                modifier = Modifier.align(Alignment.Center)
-            ) {
-                Text("Save")
-            }
-        }
-    }
 
-    @Composable
-    fun SpeechToText() {
 
-        val context = LocalContext.current
-        val noteCard = NoteCard(cardID = 0, title = "Test", snippet = "This is a test")
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
 
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            SpeechRecognizerComponent(context = context, activity = this@MainActivity, noteCard = noteCard)
-        }
 
-    }
-
-    @ExperimentalMaterialApi
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-            SpeechToText()
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MemrepoTheme {
+        Greeting("Android")
     }
 }
