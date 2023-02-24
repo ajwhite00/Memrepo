ccc69ce7436ab1e503c5e2c6214416d928bb4ce2
Nicholas-Lawson <lawsonnj@mail.uc.edu>
2023-02-24 15:05:56 -0500

    Implemented correct unit testing and dto functions from DTO branch

--- app/src/main/java/com/memrepo/dto/NoteCard.kt
+++ app/src/main/java/com/memrepo/dto/NoteCard.kt
@@ -13,18 +13,13 @@ import androidx.room.PrimaryKey
 @Entity
 data class NoteCard(@PrimaryKey(autoGenerate = true) val cardID : Int,@ColumnInfo("title") val title : String,@ColumnInfo("snippet") val snippet : String){
 
-        var snippetDisplay = this.snippet //has three rounds of replacement
-                .replace("-".toRegex(), " ") //replace hyphens with space because of two cases: split words with hyphen in between and remove hyphen in the middle of a sentence
-                .replace("  ".toRegex(), " ") //replace any double spaces caused by hyphen replacement with a single space
-                .replace("[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]".toRegex(), "") //remove punctuation and other non-letter chars
-
-        //var snippetDisplayList = snippetDisplay.split(" ").toList() create a list by splitting string on space
-        //var snippetDisplayList : List<String> = listOf<String>(this.snippetDisplay.split(" ").toString())
-
-        /**
-         * Function that can change snippet into snippetDisplay with toString() implementation.
-         */
-        override fun toString(): String {
-                return snippetDisplay
+        fun createSnippetDisplayList(): List<String> {
+                return this.snippet //has three rounds of replacement
+                        .replace("-".toRegex(), " ") //replace hyphens with space because of two cases: split words with hyphen in between and remove hyphen in the middle of a sentence
+                        .replace("  ".toRegex(), " ") //replace any double spaces caused by hyphen replacement with a single space
+                        .replace("[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]".toRegex(), "") //remove punctuation and other non-letter chars
+                        .lowercase() //change everything to lowercase to be used later for mic input comparison
+                        .split(" ") //separates words as space
+                        .toList() //creates a list individual words to be used later for mic input comparison
         }
 }
