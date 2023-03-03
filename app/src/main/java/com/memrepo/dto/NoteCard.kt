35adb67751648bd7e6454d585f9e36ed9fdc210e
RileySetser <setserr7@gmail.com>
2023-03-02 20:37:54 -0500

    Change regular comments into documentation comments.

--- app/src/main/java/com/memrepo/dto/NoteCard.kt
+++ app/src/main/java/com/memrepo/dto/NoteCard.kt
@@ -11,10 +11,18 @@ data class NoteCard(
         @ColumnInfo("snippet") val snippet : String)
 {
 
-        var snippetDisplay = this.snippet //has three rounds of replacement
-                .replace("-".toRegex(), " ") //replace hyphens with space because of two cases: split words with hyphen in between and remove hyphen in the middle of a sentence
-                .replace("  ".toRegex(), " ") //replace any double spaces caused by hyphen replacement with a single space
-                .replace("[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]".toRegex(), "") //remove punctuation and other non-letter chars
+        /**
+         * Has three rounds of replacement:
+         * - Replaces hyphens with spaces because of two cases:
+         *      -- Split words with hyphens in between.
+         *      -- Remove hyphens in the middle of a sentence.
+         * - Replaces any double spaces caused by replacing hyphens with a single space.
+         * - Remove any punctuation and other non-letter characters.
+         */
+        var snippetDisplay = this.snippet
+                .replace("-".toRegex(), " ")
+                .replace("  ".toRegex(), " ")
+                .replace("[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]".toRegex(), "")
 
         //var snippetDisplayList = snippetDisplay.split(" ").toList() create a list by splitting string on space
         //var snippetDisplayList : List<String> = listOf<String>(this.snippetDisplay.split(" ").toString())
