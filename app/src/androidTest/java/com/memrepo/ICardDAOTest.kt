70989ee5b5888293cb2862ecad72a6aa3d963473
patel6rv <patel6rv@mail.uc.edu>
2023-03-23 11:42:55 -0400

    Create instantTaskExecutor

--- app/src/androidTest/java/com/memrepo/ICardDAOTest.kt
+++ app/src/androidTest/java/com/memrepo/ICardDAOTest.kt
@@ -1,17 +1,27 @@
 package com.memrepo
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
+import androidx.test.filters.SmallTest
 import com.memrepo.dao.ICardDAO
 import com.memrepo.dao.NoteCardDatabase
 import com.memrepo.dto.NoteCard
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
+@SmallTest
 class ICardDAOTest {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
     private lateinit var noteCardDatabase: NoteCardDatabase
     private lateinit var iCardDAO: ICardDAO
 
