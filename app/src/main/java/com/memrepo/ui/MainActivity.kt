0086540f135e48c4a7a675836989fc977aa8cfa2
Pruthvi6797 <patel5pu@mail.uc.edu>
2023-03-23 14:08:28 -0400

    Close Icon Done

--- app/src/main/java/com/memrepo/ui/MainActivity.kt
+++ app/src/main/java/com/memrepo/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.memrepo
+package com.memrepo.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
@@ -17,9 +17,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.memrepo.R
 import com.memrepo.ui.theme.MemrepoTheme
 import kotlinx.coroutines.launch
 
@@ -28,185 +31,184 @@ class MainActivity : ComponentActivity() {
     @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-<<<<<<<<< Temporary merge branch 1
         setContentView(R.layout.activity_main)
 
         setContent {
-=========
->>>>>>>>> Temporary merge branch 2
-            setContent {
-                MemrepoTheme {
-                    // A surface container using the 'background' color from the theme
-                    Surface(
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colors.background
-                    ) {
-                        MainScreen()
-                    }
-                }
-            }  
-    }
-    @ExperimentalMaterialApi
-    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-    @Composable
-    fun MainScreen() {
-
-
-=======
-        // Bottom Sheet is used to create a 'Modal' but for android apps
-        val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
-            bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed)
-        )
-        val coroutineScope = rememberCoroutineScope()
-
-        BottomSheetScaffold(
-            scaffoldState = bottomSheetScaffoldState,
-            sheetContent = {
-                Box(
-                    Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight()
+            MemrepoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
                 ) {
-                    Column {
-                        // Contents of the Bottom Sheet come from this component
-                        AddSnippet(bottomSheetScaffoldState)
-                    }
+                    MainScreen()
                 }
-            }, sheetPeekHeight = 0.dp
-        ) {
-            Scaffold(
-                // Bar across top of screen
-                topBar = {
-                    TopAppBar(
-                        title = {
-                            Text(
-                                "MemRepo"
-                            )
-                        },
-                    )
-                },
-                // Bar across bottom of screen
-                bottomBar = {
-                    BottomAppBar {
-                        Box(Modifier.fillMaxWidth()) {
-                            Button(
-                                onClick = {
-                                    // When the button is clicked the bottom sheet will expand or collapse
-                                    coroutineScope.launch {
-                                        if (bottomSheetScaffoldState.bottomSheetState.isCollapsed) {
-                                            bottomSheetScaffoldState.bottomSheetState.expand()
-                                        } else {
-                                            bottomSheetScaffoldState.bottomSheetState.collapse()
-                                        }
-                                    }
-                                },
-                                modifier = Modifier
-                                    .align(Alignment.Center)
-                                    .fillMaxWidth()
-                                    .fillMaxHeight()
-                            ) {
-                                (Text(text = "Add"))
-                            }
+            }
+        }
+    }
+
+        @ExperimentalMaterialApi
+        @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+        @Composable
+        fun MainScreen() {
+
+
+            // Bottom Sheet is used to create a 'Modal' but for android apps
+            val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+                bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed)
+            )
+            val coroutineScope = rememberCoroutineScope()
+
+            BottomSheetScaffold(
+                scaffoldState = bottomSheetScaffoldState,
+                sheetContent = {
+                    Box(
+                        Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                    ) {
+                        Column {
+                            // Contents of the Bottom Sheet come from this component
+                            AddSnippet(bottomSheetScaffoldState)
                         }
                     }
-                },
-                // The content in between the top bar and bottom bar
-                content = {
-                    MyContent()
-                }
-            )
-        }
-    }
-    @Composable
-    fun MyContent() {
-        val paddingModifier = Modifier.padding(10.dp)
-        Box(Modifier.fillMaxSize()) {
-            Card(
-                shape = RoundedCornerShape(20.dp),
-                elevation = 10.dp,
-                modifier = Modifier.padding(10.dp)
+                }, sheetPeekHeight = 0.dp
             ) {
-                Column(paddingModifier) {
-                    // Title and Snippet are placeholders for now, eventually these will be injected values from the database
-                    Text(text = "Title", Modifier.fillMaxWidth())
-                    Text(text = "Snippet", Modifier.fillMaxWidth())
-                }
-                // Button will have the options to Edit or delete the note card
-                Button(modifier = Modifier.align(Alignment.TopEnd), onClick = {}){
-                    Text("...")
-                }
+                Scaffold(
+                    // Bar across top of screen
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    "MemRepo"
+                                )
+                            },
+                        )
+                    },
+                    // Bar across bottom of screen
+                    bottomBar = {
+                        BottomAppBar {
+                            Box(Modifier.fillMaxWidth()) {
+                                Button(
+                                    onClick = {
+                                        // When the button is clicked the bottom sheet will expand or collapse
+                                        coroutineScope.launch {
+                                            if (bottomSheetScaffoldState.bottomSheetState.isCollapsed) {
+                                                bottomSheetScaffoldState.bottomSheetState.expand()
+                                            } else {
+                                                bottomSheetScaffoldState.bottomSheetState.collapse()
+                                            }
+                                        }
+                                    },
+                                    modifier = Modifier
+                                        .align(Alignment.Center)
+                                        .fillMaxWidth()
+                                        .fillMaxHeight()
+                                ) {
+                                    (Text(text = "Add"))
+                                }
+                            }
+                        }
+                    },
+                    // The content in between the top bar and bottom bar
+                    content = {
+                        MyContent()
+                    }
+                )
             }
         }
-    }
 
-    @OptIn(ExperimentalComposeUiApi::class)
-    @ExperimentalMaterialApi
-    @Composable
-    fun AddSnippet(bottomSheetScaffoldState: BottomSheetScaffoldState) {
-        var title by remember { mutableStateOf("") }
-        var text by remember { mutableStateOf("") }
-        val coroutineScope = rememberCoroutineScope()
-        val keyBoardController = LocalSoftwareKeyboardController.current
-        Box (modifier = Modifier.fillMaxWidth()) {
-            // When Exit button is clicked collapse the bottom sheet, hide the key board, and set the text fields to empty
-            Button(
-                onClick = {
-                    coroutineScope.launch { bottomSheetScaffoldState.bottomSheetState.collapse() }
-                    keyBoardController?.hide()
-                    title = ""
-                    text = ""
-                },
-                modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .padding(end = 10.dp)
-            ) {
-                Text("Exit")
+        @Composable
+        fun MyContent() {
+            val paddingModifier = Modifier.padding(10.dp)
+            Box(Modifier.fillMaxSize()) {
+                Card(
+                    shape = RoundedCornerShape(20.dp),
+                    elevation = 10.dp,
+                    modifier = Modifier.padding(10.dp)
+                ) {
+                    Column(paddingModifier) {
+                        // Title and Snippet are placeholders for now, eventually these will be injected values from the database
+                        Text(text = "Title", Modifier.fillMaxWidth())
+                        Text(text = "Snippet", Modifier.fillMaxWidth())
+                    }
+                    // Button will have the options to Edit or delete the note card
+                    Button(modifier = Modifier.align(Alignment.TopEnd), onClick = {}) {
+                        Text("...")
+                    }
+                }
             }
         }
-        Box (modifier = Modifier.fillMaxWidth()) {
-            Column (modifier = Modifier.align(Alignment.Center)){
-                OutlinedTextField(
-                    value = title,
-                    onValueChange = { title = it },
-                    label = { Text("Title") },
+
+        @OptIn(ExperimentalComposeUiApi::class)
+        @ExperimentalMaterialApi
+        @Composable
+        fun AddSnippet(bottomSheetScaffoldState: BottomSheetScaffoldState) {
+            var title by remember { mutableStateOf("") }
+            var text by remember { mutableStateOf("") }
+            val coroutineScope = rememberCoroutineScope()
+            val keyBoardController = LocalSoftwareKeyboardController.current
+            Box(modifier = Modifier.fillMaxWidth()) {
+                // When Exit button is clicked collapse the bottom sheet, hide the key board, and set the text fields to empty
+                Button(
+                    onClick = {
+                        coroutineScope.launch { bottomSheetScaffoldState.bottomSheetState.collapse() }
+                        keyBoardController?.hide()
+                        title = ""
+                        text = ""
+                    },
                     modifier = Modifier
-                        .padding(bottom = 10.dp)
-                        .widthIn(max = 300.dp),
-                    singleLine = true
-                )
-                OutlinedTextField(
-                    value = text,
-                    onValueChange = { text = it },
-                    label = { Text("Snippet Text") },
-                    modifier = Modifier
-                        .height(300.dp)
-                        .widthIn(max = 300.dp)
-                )
+                        .align(Alignment.TopEnd)
+                        .padding(end = 10.dp)
+                ) {
+                    Icon(painter = painterResource(id = R.drawable.ic_close_foreground),
+                    contentDescription = "Close")
+                }
+            }
+            Box(modifier = Modifier.fillMaxWidth()) {
+                Column(modifier = Modifier.align(Alignment.Center)) {
+                    OutlinedTextField(
+                        value = title,
+                        onValueChange = { title = it },
+                        label = { Text("Title") },
+                        modifier = Modifier
+                            .padding(bottom = 10.dp)
+                            .widthIn(max = 300.dp),
+                        singleLine = true
+                    )
+                    OutlinedTextField(
+                        value = text,
+                        onValueChange = { text = it },
+                        label = { Text("Snippet Text") },
+                        modifier = Modifier
+                            .height(300.dp)
+                            .widthIn(max = 300.dp)
+                    )
+                }
+            }
+            Box(modifier = Modifier.fillMaxWidth()) {
+                // When the Save button is clicked collapse the bottom sheet, hide the keyboard, save the fields to database
+                Button(
+                    // Logic to save snippet
+                    /* TODO */
+                    onClick = {
+                        coroutineScope.launch { bottomSheetScaffoldState.bottomSheetState.collapse() }
+                        keyBoardController?.hide()
+                    },
+                    modifier = Modifier.align(Alignment.Center)
+                ) {
+                    Text("Save")
+                }
             }
         }
-        Box ( modifier = Modifier.fillMaxWidth() ){
-            // When the Save button is clicked collapse the bottom sheet, hide the keyboard, save the fields to database
-            Button (
-                // Logic to save snippet
-                /* TODO */
-                onClick = {
-                    coroutineScope.launch { bottomSheetScaffoldState.bottomSheetState.collapse() }
-                    keyBoardController?.hide()
-                },
-                modifier = Modifier.align(Alignment.Center)
-            ) {
-                Text("Save")
+
+        @ExperimentalMaterialApi
+        @Preview(showBackground = false)
+        @Composable
+        fun DefaultPreview() {
+            MemrepoTheme {
+                Column {
+                    MainScreen()
+                }
             }
         }
     }
-    @ExperimentalMaterialApi
-    @Preview(showBackground = false)
-    @Composable
-    fun DefaultPreview() {
-        MemrepoTheme {
-            Column {
-                MainScreen()
-            }
-        }
-    }
-}
