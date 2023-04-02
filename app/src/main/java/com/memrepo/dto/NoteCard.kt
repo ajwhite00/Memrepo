828c61d21be7a841f5ae8defc855f6abe4a82068
tracyjn@mail.uc.edu <tracyjn@mail.uc.edu>
2023-04-02 18:28:55 -0400

    made suggestions for NoteCard dto

--- app/src/main/java/com/memrepo/dto/NoteCard.kt
+++ app/src/main/java/com/memrepo/dto/NoteCard.kt
@@ -11,13 +11,16 @@ import androidx.room.PrimaryKey
  * title and snippet are both components of a NoteCard that is a stored string for the User.
  */
 @Entity(tableName = "NoteCards")
-data class NoteCard(@PrimaryKey(autoGenerate = true) val cardID : Int,@ColumnInfo("title") val title : String,@ColumnInfo("snippet") val snippet : String){
-
+data class NoteCard(
+        @PrimaryKey(autoGenerate = true) val cardId: Int,
+        @ColumnInfo(name = "title") val title: String,
+        @ColumnInfo(name = "snippet") val snippet: String
+) {
         fun createSnippetDisplayList(): MutableList<String> {
-                return this.snippet //has three rounds of replacement
-                        .replace("-".toRegex(), " ") //replace hyphens with space because of two cases: split words with hyphen in between and remove hyphen in the middle of a sentence
-                        .replace("  ".toRegex(), " ") //replace any double spaces caused by hyphen replacement with a single space
-                        .replace("[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]".toRegex(), "") //remove punctuation and other non-letter chars
+                return this.snippet
+                        .replace("-", " ") //replace hyphens with space because of two cases: split words with hyphen in between and remove hyphen in the middle of a sentence
+                        .replace(Regex(" {2,}"), " ") //replace any double spaces caused by hyphen replacement with a single space
+                        .replace(Regex("[^a-zA-Z ]"), "") //remove punctuation and other non-letter chars
                         .lowercase() //change everything to lowercase to be used later for mic input comparison
                         .split(" ") //separates words as space
                         .toMutableList() //creates a list individual words to be used later for mic input comparison
