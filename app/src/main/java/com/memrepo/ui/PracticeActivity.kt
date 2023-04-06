d0d2aec61d33a35958ad4a8f38fd6a9346c20dcd
ajwhite00 <aaronjwhite00@gmail.com>
2023-04-05 21:17:23 -0400

    Added import

--- app/src/main/java/com/memrepo/ui/PracticeActivity.kt
+++ app/src/main/java/com/memrepo/ui/PracticeActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.memrepo.SpeechRecognizerComponent
 import com.memrepo.dto.NoteCard
 import com.memrepo.ui.theme.MemrepoTheme
